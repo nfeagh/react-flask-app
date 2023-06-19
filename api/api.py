2877a34fb3aba7bd6ae16b59b49fcd1cd92b7ba9
Ece Deniz <ecedenniz@gmail.com>
2023-06-19 12:26:21 +0000

    fix

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask
-from recommend import get_recommendations
+import recommend
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
@@ -24,18 +24,18 @@ def get_movies_by_id(searchValue):
     url = "http://www.omdbapi.com/?i=" + str(searchValue) + "&apikey=2651b0db"
     response = requests.get(url=url)
     return response.json()
-
-
-@app.route('/movies/recommend/<searchValue>')
-def get_recommendation(searchValue):
-    b = get_recommendations(searchValue)
-    response = []
-    for id in b:
-        response.append(get_movies_by_id(id))
-    return response
-
 @app.route('/movies/<searchValue>/<year>')
 def get_movies_by_year(searchValue, year):
     url = "https://www.omdbapi.com/?s=" + str(searchValue) + "&y=" + str(year) + "&apikey=263d22d8"
     response = requests.get(url=url)
     return response.json()
+
+@app.route('/movies/recommend/<searchValue>')
+def get_recommendation(searchValue):
+    b = recommend.get_recommendations(searchValue)
+    response = []
+    for id in b:
+        response.append(get_movies_by_id(id))
+    return response
+
+get_recommendation("tt0110357")
